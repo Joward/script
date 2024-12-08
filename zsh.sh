0608bf51d183557fa63ae05e9bcf6c8f6265ad8e
MacXiong <mxiong@hotmail.com>
2024-12-07 17:43:40 -0800

    daily update script files

--- zsh.sh
+++ zsh.sh
@@ -3,8 +3,20 @@
 # exit on error
 set -e
 
-sudo apt update
-sudo apt install zsh
+# Ubuntu
+# sudo apt update
+# sudo apt install zsh
+# chsh -s /usr/bin/zsh
 
-chsh -s /usr/bin/zsh
+# CentOS
+# update Repo
+
+sudo yum install zsh
+chsh -s /bin/zsh
+
 echo $SHELL
+
+sh -c "$(wget -O- https://gitee.com/pocmon/ohmyzsh/raw/master/tools/install.sh)"
+
+# theme : bira
+# plugin : auto-complete
